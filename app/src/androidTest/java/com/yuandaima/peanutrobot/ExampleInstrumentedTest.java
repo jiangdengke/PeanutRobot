43672590439bd3a123b65e9eb94e8b6726ae223f
jiangdengke <jiang1728439852@gmail.com>
2026-05-21 18:34:36 +0800

    :wrench: chore(android): 切换授权包名

--- app/src/androidTest/java/com/yuandaima/peanutrobot/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/yuandaima/peanutrobot/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.yuandaima.peanutrobot", appContext.getPackageName());
+        assertEquals("com.keenon.zealq", appContext.getPackageName());
     }
 }
